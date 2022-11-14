79390b4a4530080ed1f7e07997ba3981cbab636c
r0x16 <sir.thomas.lara@gmail.com>
2022-11-14 18:33:42 -0300

    Add bussiness logic to list all devices
    - Added devices list service
    - Added device repository interface
    - Added repository filter interface to create advanced queries

---
+++ src/devices/app/list.devices.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"github.com/r0x16/ThunderForce/src/devices/domain/model"
+	"github.com/r0x16/ThunderForce/src/devices/domain/repository"
+	"github.com/r0x16/ThunderForce/src/shared/domain"
+)
+
+/**
+* Application service for listing devices
+**/
+type DeviceLister struct {
+	deviceRepository repository.DeviceRepository
+}
+
+/**
+* Creates a DeviceLister use case with the given DeviceRepository
+* @param devices the DeviceRepository to use
+* @return a DeviceLister instance
+**/
+func ListDevices(devices repository.DeviceRepository) *DeviceLister {
+	return &DeviceLister{
+		deviceRepository: devices,
+	}
+}
+
+/**
+* Lists all devices
+* @return a list of devices
+**/
+func (lister *DeviceLister) List() ([]*model.Device, error) {
+	devices, err := lister.deviceRepository.FindAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return devices, nil
+}
+
+/**
+* Gets a filterted list of devices
+* @param filter the filter to apply
+* @return a list of devices
+**/
+func (lister *DeviceLister) Filter(filter domain.RepositoryFilter) ([]*model.Device, error) {
+	devices, err := lister.deviceRepository.FindFiltered(filter)
+	if err != nil {
+		return nil, err
+	}
+
+	return devices, nil
+}
---
+++ src/devices/domain/repository/DeviceRepository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"github.com/r0x16/ThunderForce/src/devices/domain/model"
+	"github.com/r0x16/ThunderForce/src/shared/domain"
+)
+
+/**
+ * This interface represents a repository for devices
+ * @param FindAll gets all devices
+ * @param FindFiltered gets a filtered list of devices
+ * @param FindById gets a device by its id
+ * @param FindByName gets a device by its name
+ * @param FindByIP gets a device by its IP
+ * @param Store persists a device
+ * @param Delete deletes a device
+**/
+type DeviceRepository interface {
+	FindAll() ([]*model.Device, error)
+	FindFiltered(filter domain.RepositoryFilter) ([]*model.Device, error)
+	FindById(id string) (*model.Device, error)
+	FindByIP(ip string) (*model.Device, error)
+	FindByName(name string) (*model.Device, error)
+	Store(device *model.Device) error
+	Update(device *model.Device) error
+	Delete(device *model.Device) error
+}
