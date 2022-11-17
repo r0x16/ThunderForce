3e91a12280369b0561cefd48badddd31cb20d9d9
r0x16 <sir.thomas.lara@gmail.com>
2022-11-17 16:13:58 -0300

    Fix device default route

--- src/devices/infraestructure/module/DevicesModule.go
+++ src/devices/infraestructure/module/DevicesModule.go
@@ -23,14 +23,10 @@ var _ domain.ApplicationModule = &DevicesModule{}
 func (m *DevicesModule) Setup() {
 	devices := m.Bundle.Server.Group("/devices")
 
-	devices.GET("/", m.Bundle.ActionInjection(action.ListDevicesAction))
-
+	devices.GET("", m.Bundle.ActionInjection(action.ListDevicesAction))
 	devices.GET("/:id", m.Bundle.ActionInjection(action.ShowDevicesAction))
-
 	devices.POST("/", m.Bundle.ActionInjection(action.StoreDevicesAction))
-
 	devices.PUT("/:id", m.Bundle.ActionInjection(action.UpdateDevicesAction))
-
 	devices.DELETE("/:id", m.Bundle.ActionInjection(action.DeleteDevicesAction))
 
 }
